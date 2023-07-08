c90e6ee8e096bb4c6c0e59ff2b8c25fbca8c5711
Kodake <kodake@hotmail.es>
2023-07-07 21:16:47 -0400

    Solved cannot convert undefined value to object

--- store/sharedStateStore.tsx
+++ store/sharedStateStore.tsx
@@ -57,28 +57,30 @@ class SharedStateStore {
       runInAction(() => {
         this.criptomonedas = Data;
       });
-    })
-      .catch((error) => {
-        console.error(error);
-      });
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   async cotizarCriptomoneda(): Promise<void> {
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${this.criptomoneda}&tsyms=${this.moneda}`;
-    await axios.get(url).then(resp => {
-      const data = resp.data.DISPLAY[this.criptomoneda][this.moneda];
-      runInAction(() => {
-        this.setCargando(true);
-        setTimeout(() => {
-          this.setResultado(data);
-          this.setConsultarAPI(false);
-          this.setCargando(false);
-        }, 3000);
-      });
-    })
-      .catch((error) => {
+    if (this.criptomoneda !== '' && this.moneda !== '') {
+      await axios.get(url).then(resp => {
+        const data = resp.data.DISPLAY[this.criptomoneda][this.moneda];
+        console.log(data);
+
+        runInAction(() => {
+          this.setCargando(true);
+          setTimeout(() => {
+            this.setResultado(data);
+            this.setConsultarAPI(false);
+            this.setCargando(false);
+          }, 3000);
+        });
+      }).catch((error) => {
         console.error(error);
       });
+    }
   }
 }
 
