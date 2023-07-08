830a7f0ca1cca6089fd78d87594f47536f8f7ef9
Kodake <kodake@hotmail.es>
2023-07-08 01:44:14 -0400

    Removed unused prop interface

--- interfaces/appInterfaces.tsx
+++ interfaces/appInterfaces.tsx
@@ -13,19 +13,3 @@ export interface Resultado {
     CHANGEPCT24HOUR: number;
     LASTUPDATE: string;
 }
-
-export interface SharedStateStore {
-    moneda: string;
-    criptomoneda: string;
-    consultarAPI: boolean;
-    resultado: any;
-    cargando: boolean;
-    criptomonedas: Criptomoneda[];
-    setMoneda(moneda: string): void;
-    setCriptomoneda(criptomoneda: string): void;
-    setConsultarAPI(consultarAPI: boolean): void;
-    setResultado(resultado: any): void;
-    setCargando(cargando: boolean): void;
-    fetchCriptomonedas(): Promise<void>;
-    cotizarCriptomoneda(): Promise<void>;
-}
